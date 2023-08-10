470472793c6bef786ad68a118472de16db6965cb
JagdishTholiya1 <139554965+JagdishTholiya1@users.noreply.github.com>
2023-08-10 22:22:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use('/api/v1', require('./routes/Transactions.js' ))
 
 app.get("/",(req,res)=>{
     res.setHeader("Access-Control-Allow-Credentials","true")
+    res.send("api is running");
 })
 
 const server = () => {
